b87cef5c3713eaa91e9d7333935dced7de0954cd
Felipe Coutinho <fcoelho.mrc@gmail.com>
2026-03-07 03:28:34 +0000

    initial commit

---
+++ services/backend/models.py
@@ -0,0 +1,10 @@
+MODELS = {
+    "bge": {
+        "hf_name": "BAAI/bge-large-en-v1.5",
+        "batch_size": 64,
+        "query_prompt": "Represent this sentence for searching relevant passages: ",
+        "doc_prompt": None,
+        "description": "bge-large-en-v1.5",
+        "embed_size": 1024,
+    }
+}
