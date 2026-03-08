6879f24bb280cc3b5d9a9de2b84633479c015194
Felipe Coutinho <fcoelho.mrc@gmail.com>
2026-03-08 01:12:52 +0000

    refactor: cleaner class api + extracted shared behavior

--- services/data/fetcher.py
+++ services/data/fetcher.py
@@ -3,46 +3,29 @@ import time
 from concurrent.futures import ThreadPoolExecutor
 from datetime import datetime
 from pathlib import Path
-from typing import Any, Dict, Optional
+from typing import Any, Dict, Iterator, Optional
 
 import requests
 from retry import retry
-from sqlalchemy import create_engine, exists, select
+from sqlalchemy import exists, select
 from sqlalchemy.dialects.postgresql import insert
 from sqlalchemy.orm import Session
 
+from services.data.interface import PostgresInterface
 from services.data.orm import Document, Object
 from services.data.templates import DocumentTemplate
 
 SEMANTIC_SCHOLAR_API_KEY = os.environ.get("SEMANTIC_SCHOLAR_API_KEY")
 BASE_URL = "https://api.semanticscholar.org/graph/v1/paper/search/bulk"
-
 FIELDS = "paperId,title,abstract,authors,venue,year,publicationDate,url,openAccessPdf,citationCount,influentialCitationCount,fieldsOfStudy"
 
 
-# TODO: Extract a base Fetcher class (connect, rate_limit, ...)
-class SemanticScholarFetcher:
+class SemanticScholarFetcher(PostgresInterface):
     def __init__(self):
-        self.engine = self.connect()
-
-    @staticmethod
-    def connect():
-        user = os.environ["POSTGRES_USER"]
-        password = os.environ["POSTGRES_PASSWORD"]
-        db = os.environ["POSTGRES_DB"]
-
-        engine = create_engine(
-            f"postgresql+psycopg2://{user}:{password}@localhost/{db}",
-            echo=True,
-        )
-        return engine
-
-    @staticmethod
-    def rate_limit(wait):
-        time.sleep(wait)
+        super().__init__()
 
     @retry(tries=8, delay=1, backoff=2, jitter=(2, 6))
-    def request(self, url: str, params: Dict, headers: Dict) -> Dict[str, Any]:
+    def send_request(self, url: str, params: Dict, headers: Dict) -> Dict[str, Any]:
         response = requests.get(url, params=params, headers=headers, timeout=90)
         status_code = response.status_code
         if status_code == 200:
@@ -50,12 +33,29 @@ class SemanticScholarFetcher:
         else:
             raise requests.HTTPError(status_code)
 
-    def sanitize(self, text: str) -> str:
-        return (
-            text.replace(" ", "_").replace("(", "").replace(")", "").replace("/", "_")
-        )
+    def send_batched_request(
+        self, url: str, params: Dict, headers: Dict
+    ) -> Iterator[list[Dict[str, Any]]]:
+        token: Optional[str] = None
+        while True:
+            _params = params.copy()
+            if token:
+                _params["token"] = token
 
-    def write_documents(self, documents: list[Dict[str, Any]]):
+            try:
+                data = self.send_request(url=url, params=_params, headers=headers)
+            except Exception:
+                break
+
+            response = data.get("data", [])
+            token = data.get("token")  # None when done
+            yield response
+
+            if not token or len(response) == 0:
+                break
+            self.rate_limit(1)
+
+    def write_to_documents(self, documents: list[Dict[str, Any]]):
         rows = [DocumentTemplate.from_dict(d).model_dump() for d in documents]
         with Session(self.engine) as session:
             session.execute(
@@ -64,7 +64,12 @@ class SemanticScholarFetcher:
             )
             session.commit()
 
-    def request_batch(
+    def sanitize(self, text: str) -> str:
+        return (
+            text.replace(" ", "_").replace("(", "").replace(")", "").replace("/", "_")
+        )
+
+    def execute(
         self, venues: list[str], start_year: int, end_year: int | None, query: str = ""
     ) -> None:
         if end_year is None:
@@ -75,56 +80,29 @@ class SemanticScholarFetcher:
         )
 
         for venue in venues:
-            token: Optional[str] = None
-            params_base = {
+            params = {
                 "query": query,
                 "venue": self.sanitize(venue),
                 "year": f"{start_year}-{end_year}",
                 "fields": FIELDS,
             }
-
-            while True:
-                params = params_base.copy()
-                if token:
-                    params["token"] = token
-
-                try:
-                    data = self.request(url=BASE_URL, params=params, headers=headers)
-                except Exception:
-                    break
-
-                documents = data.get("data", [])
-                token = data.get("token")  # None when done
-                self.write_documents(documents)
-                if not token or len(documents) == 0:
-                    break
-                self.rate_limit(1)
+            iterator = self.send_batched_request(
+                url=BASE_URL,
+                params=params,
+                headers=headers,
+            )
+            for chunk in iterator:
+                self.write_to_documents(chunk)
 
 
-class PdfFetcher:
+# FIXME: replace prints by proper logging
+class PdfFetcher(PostgresInterface):
     def __init__(self, max_workers: int, store_root: str):
+        super().__init__()
         self.max_workers = max_workers
         self.store_root = store_root
 
-        self.engine = self.connect()
-
-    @staticmethod
-    def connect():
-        user = os.environ["POSTGRES_USER"]
-        password = os.environ["POSTGRES_PASSWORD"]
-        db = os.environ["POSTGRES_DB"]
-
-        engine = create_engine(
-            f"postgresql+psycopg2://{user}:{password}@localhost/{db}",
-            echo=True,
-        )
-        return engine
-
-    @staticmethod
-    def rate_limit(wait):
-        time.sleep(wait)
-
-    def query(self) -> list[tuple[int, str, Path]]:
+    def query_documents(self) -> list[tuple[int, str, Path]]:
         stmt = (
             select(Document.id, Document.s2_paper_id, Document.pdf_url)
             .where(Document.pdf_url.is_not(None))
@@ -144,7 +122,7 @@ class PdfFetcher:
 
     @staticmethod
     @retry(tries=3, delay=1, backoff=2)
-    def download(url: str) -> bytes:
+    def send_request(url: str) -> bytes:
         print(f"Downloading {url}...")
         try:
             response = requests.get(url, timeout=90)
@@ -158,31 +136,28 @@ class PdfFetcher:
             raise requests.HTTPError(status_code)
 
     @staticmethod
-    def write(content: bytes, path: Path):
+    def write_to_disk(content: bytes, path: Path):
         print(f"Saving to {str(path)}...")
         tmp = path.with_suffix(".tmp")
         tmp.write_bytes(content)
         tmp.rename(path)
 
-    def register(self, doc_id: int, path: Path):
+    def write_to_objects(self, doc_id: int, path: Path):
         print(f"Writting record...")
         obj = Object(doc_id=doc_id, path=str(path))
         with Session(self.engine) as session:
             session.add(obj)
             session.commit()
 
-    def process(self, doc_id, url, path):
+    def task(self, doc_id, url, path):
         try:
-            content = self.download(url)
-            self.write(content, path)
-            self.register(doc_id, path)
+            content = self.send_request(url)
+            self.write_to_disk(content, path)
+            self.write_to_objects(doc_id, path)
         except Exception as error:
             print(f"{error} -> ({doc_id}, {url}, {path})")
 
     def execute(self):
-        params = self.query()
-        # for p in params:  # doc_id, url, path
-        #     print(p)
-        #     self.process(*p)
+        params = self.query_documents()
         with ThreadPoolExecutor(max_workers=self.max_workers) as executor:
-            executor.map(lambda p: self.process(*p), params)
+            executor.map(lambda p: self.task(*p), params)
