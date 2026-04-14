0d919d6e7357ad1cbcf4b21f3a0b8fbe63926fe1
Felipe Coutinho <69319549+fcoelhomrc@users.noreply.github.com>
2026-04-14 16:33:14 +0200

    resume dev

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 import os
 from pathlib import Path
+from dotenv import load_dotenv
 
 from services.data.fetcher import PdfFetcher, SemanticScholarFetcher
+
+
+load_dotenv()
+
 
 # FIXME: Handle this properly e.g. at Docker Compose
 OBJECT_STORE_ROOT = os.environ.get("OBJECT_STORE_ROOT", "")
@@ -47,7 +52,7 @@ venues = [
     "Radiology: Artificial Intelligence",
 ]
 
-# fetcher.request_batch(
+# fetcher.execute(
 #     venues=venues,
 #     start_year=2022,
 #     end_year=None,
